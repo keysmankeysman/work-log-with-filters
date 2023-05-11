e6894cc97ec7538f7d4fffb27bcbf4a47ab9d598
Home <home@MacBook-Pro-Home.local>
2023-05-11 10:57:19 +0300

    some

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-let btnName = document.querySelector('#btn-name'),
-	btnFilter = document.querySelector('.btn-filter'),
+let btnFilter = document.querySelector('.btn-filter'),
 	hideMenu = document.querySelector('.hide-menu'),
 	allWorkers = document.querySelectorAll('.table-worker'),
 	inputSearch = document.querySelector('#searchByName'),
@@ -8,8 +7,8 @@ let btnName = document.querySelector('#btn-name'),
 
 
 // let selectedStatuses = ['inwork', 'stop', 'rework', 'verified', 'done']
-// let selectedStatuses = ['execute', 'allHistory', 'onlyWork']
 let selectedStatuses = []
+// let selectedStatuses = []
 
 const statuses = {
 	execute: 'Выполнение',
@@ -19,35 +18,73 @@ const statuses = {
 
 checkboxes.forEach(checkbox => {
 	checkbox.addEventListener('click', () => {
-		if (selectedStatuses.includes(checkbox.value)) {
-			if (!selectedStatuses.length) {
-				showAllTableBody()
-			} else {
-				showCurrenTableBody(checkbox.value)
-			}
-			let index = selectedStatuses.findIndex(el => el === checkbox.value)
-			selectedStatuses.splice(index, 1)
-		} else {
-			selectedStatuses.push(checkbox.value)
-			filterStatus(checkbox.value) 
-		}
+		switch(checkbox.value) {
+			case 'execute':  
+				executeFilter()
+			break
+			case 'allHistory': 
+				allHistoryFilter()
+			break
+			case 'onlyWork':
+				onlyWorkFilter()
+			break
+		  }
 	})
 })
 
-btnName.addEventListener('click', () => {
-	if (inputSearch.value) {
-		allWorkers.forEach(worker => {
-			// let innerCaption = worker.querySelector('.table-caption')
-			let innerCaptionHTML = worker.querySelector('.table-caption').innerHTML
-			if (innerCaptionHTML.includes(inputSearch.value)) {
-				worker.classList.add('hidden')
-			}
-		})
-	} else {
-		showAllWorkers()
-	}
-})
+function executeFilter() {
+	console.log('execute function')
+}
 
+function allHistoryFilter() {
+	allWorkers.forEach(el => {
+		let innerBody = el.querySelectorAll('.table-body')
+		innerBody.forEach((body) => {
+			body.classList.remove('hidden')
+		})
+	})
+}
+
+// только в работе, показывать статусы "В работе" и "Переделка"
+function onlyWorkFilter() {
+	let statuses = ['В работе','Переделка']
+	allWorkers.forEach(el => {
+		let innerBody = el.querySelectorAll('.table-body')
+		innerBody.forEach((body) => {
+			let currentStatus = body.querySelector('.table-body__status').innerHTML
+			if (!statuses.includes(currentStatus)) {
+				body.classList.add('hidden')
+			}
+			// if (body.querySelector('.table-body__status').innerHTML.includes(statuses)) {
+			// 	console.log('hidden')
+			// 	body.classList.add('hidden')
+			// }
+		})
+	})
+
+}
+
+
+
+
+// checkboxes.forEach(checkbox => {
+// 	checkbox.addEventListener('click', () => {
+// 		if (selectedStatuses.includes(checkbox.value)) {
+// 			if (!selectedStatuses.length) {
+// 				showAllTableBody()
+// 			} else {
+// 				showCurrenTableBody(checkbox.value)
+// 			}
+// 			let index = selectedStatuses.findIndex(el => el === checkbox.value)
+// 			selectedStatuses.splice(index, 1)
+// 		} else {
+// 			selectedStatuses.push(checkbox.value)
+// 			filterStatus(checkbox.value) 
+// 		}
+// 	})
+// })
+
+// открытие меню фильтра
 btnFilter.addEventListener('click', () => {
 	if (hideMenu.classList.contains('hidden')) {
 		hideMenu.classList.remove('hidden')
@@ -57,44 +94,39 @@ btnFilter.addEventListener('click', () => {
 
 })
 
-function showAllWorkers () {
-	allWorkers.forEach(worker => {
-		worker.classList.remove('hidden')
-	})
-}
+// function showAllWorkers () {
+// 	allWorkers.forEach(worker => {
+// 		worker.classList.remove('hidden')
+// 	})
+// }
 
-function filterStatus(value) {
-	allWorkers.forEach(el => {
-		let innerBody = el.querySelectorAll('.table-body')
-		innerBody.forEach((body) => {
-			if (body.querySelector('.table-body__status').innerHTML === statuses[value]) {
-				body.classList.add('hidden')
-			}
-		})
-	})
-}
+// function filterStatus(value) {
+// 	allWorkers.forEach(el => {
+// 		let innerBody = el.querySelectorAll('.table-body')
+// 		innerBody.forEach((body) => {
+// 			if (body.querySelector('.table-body__status').innerHTML === statuses[value]) {
+// 				body.classList.add('hidden')
+// 			}
+// 		})
+// 	})
+// }
 
 function showAllTableBody () {
-	allWorkers.forEach(el => {
-		let innerBody = el.querySelectorAll('.table-body')
-		innerBody.forEach((body) => {
-			body.classList.remove('hidden')
-		})
-	})
+
 }
 
-function showCurrenTableBody (value) {
+// function showCurrenTableBody (value) {
 
-	allWorkers.forEach(el => {
-		let innerBody = el.querySelectorAll('.table-body')
-		innerBody.forEach((body) => {
-			console.log(body)
-			if (body.querySelector('.table-body__status').innerHTML === statuses[value]) {
-				body.classList.remove('hidden')
-			}
-		})
-	})
-}
+// 	allWorkers.forEach(el => {
+// 		let innerBody = el.querySelectorAll('.table-body')
+// 		innerBody.forEach((body) => {
+// 			console.log(body)
+// 			if (body.querySelector('.table-body__status').innerHTML === statuses[value]) {
+// 				body.classList.remove('hidden')
+// 			}
+// 		})
+// 	})
+// }
 
 	
 
